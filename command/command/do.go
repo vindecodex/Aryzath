0ecd96737cbe4bbe33b0c1f4a233d9728d00fa2b
Vincent Villaluna <villalunavincent@gmail.com>
2021-01-27 15:20:12 +0800

    :rocket: the INVOKER

---
+++ command/command/do.go
@@ -0,0 +1,13 @@
+package command
+
+type Do struct {
+	c Command
+}
+
+func (d *Do) SetCommand(c Command) { // Accepts Class that implements the Command interface
+	d.c = c
+}
+
+func (d *Do) It() {
+	d.c.Execute() // Execute the method whatever the class was passed
+}
