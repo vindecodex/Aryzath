e4b8953e36b819b8e7a2ccdda457aa10616dd763
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-01 12:50:09 +0800

    :pencil: comments

--- adapter/adapter/mp4_adapter.go
+++ adapter/adapter/mp4_adapter.go
@@ -9,7 +9,7 @@ type Mp4Adapter struct {
 	Mp4 *file.Mp4
 }
 
-func (mp4Adapter *Mp4Adapter) PlayAudio() {
+func (mp4Adapter *Mp4Adapter) PlayAudio() { // since Mp4 doesn't have PlayAudio we will be the one to implement that method
 	fmt.Println("Initializing Adapter...")
 	mp4Adapter.Mp4.PlayVideo()
 }
