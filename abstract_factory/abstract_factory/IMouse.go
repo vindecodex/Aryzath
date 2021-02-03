46fa47bcc77fb1ccba17b4951fd88a2e6a74f1b1
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-03 10:39:23 +0800

    :tada: mouse interface & factory

--- abstract_factory/abstract_factory/IMouse.go
+++ abstract_factory/abstract_factory/IMouse.go
@@ -3,9 +3,9 @@ package abstract_factory
 import "fmt"
 
 type IMouse interface {
-	GetColor()
+	GetColor() string
 }
 
-func GetMouseDetails(mouse abstract_factory.IMouse) {
+func GetMouseDetails(mouse IMouse) {
 	fmt.Println(mouse.GetColor())
 }
