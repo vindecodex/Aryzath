c24036a85dbf704a8371b6397407eae2f13972cb
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-03 11:16:21 +0800

    :tada: documented!

--- abstract_factory/main.go
+++ abstract_factory/main.go
@@ -5,17 +5,21 @@ import (
 )
 
 func main() {
+	// Talks to the factory which returns an interface
 	razer, _ := abstract_factory.GetBrand("razer")
 	steelseries, _ := abstract_factory.GetBrand("steelseries")
 
+	// Created the Object by interacting with the interface
 	rmouse := razer.CreateMouse()
 	rkeyboard := razer.CreateKeyboard()
 	smouse := steelseries.CreateMouse()
 	skeyboard := steelseries.CreateKeyboard()
 
+	// Get Information through factory method that accepts interface as argument
 	abstract_factory.GetMouseDetails(rmouse)
 	abstract_factory.GetKeyboardDetails(rkeyboard)
 
+	// Get Information through factory method that accepts interface as argument
 	abstract_factory.GetMouseDetails(smouse)
 	abstract_factory.GetKeyboardDetails(skeyboard)
 }
