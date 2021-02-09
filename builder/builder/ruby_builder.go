edf96de7764ffdf1ecec8ff2d623222f582dd8d1
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-09 10:38:24 +0800

    :tada: concrete builder

---
+++ builder/builder/ruby_builder.go
@@ -0,0 +1,31 @@
+package builder
+
+import "Aryzath/builder/builder/product"
+
+type RubyBuilder struct {
+	RightArm  string
+	LeftArm   string
+	Legs      string
+	Processor string
+}
+
+func (r *RubyBuilder) SetRightArm() {
+	r.RightArm = "Right Ruby Arm"
+}
+func (r *RubyBuilder) SetLeftArm() {
+	r.LeftArm = "Left Ruby Arm"
+}
+func (r *RubyBuilder) SetLegs() {
+	r.Legs = "Ruby Legs"
+}
+func (r *RubyBuilder) SetProcessor() {
+	r.Processor = "AMD Ryzeen"
+}
+func (r *RubyBuilder) BuildCyborg() product.Cyborg {
+	return product.Cyborg{
+		RightArm:  r.RightArm,
+		LeftArm:   r.LeftArm,
+		Legs:      r.Legs,
+		Processor: r.Processor,
+	}
+}
