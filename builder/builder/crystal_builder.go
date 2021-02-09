fc33ebb8e720dee0b437b1250fc2396baa92b360
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-09 10:37:32 +0800

    :tada: concrete builder

---
+++ builder/builder/crystal_builder.go
@@ -0,0 +1,31 @@
+package builder
+
+import "Aryzath/builder/builder/product"
+
+type CrystalBuilder struct {
+	RightArm  string
+	LeftArm   string
+	Legs      string
+	Processor string
+}
+
+func (c *CrystalBuilder) SetRightArm() {
+	c.RightArm = "right crystal arm"
+}
+func (c *CrystalBuilder) SetLeftArm() {
+	c.LeftArm = "left crystal arm"
+}
+func (c *CrystalBuilder) SetLegs() {
+	c.Legs = "crystal legs"
+}
+func (c *CrystalBuilder) SetProcessor() {
+	c.Processor = "AMD Ryzeen"
+}
+func (c *CrystalBuilder) BuildCyborg() product.Cyborg {
+	return product.Cyborg{
+		RightArm:  c.RightArm,
+		LeftArm:   c.LeftArm,
+		Legs:      c.Legs,
+		Processor: c.Processor,
+	}
+}
