5acbc5f2e1eea7c29dc8e16f9aa7ae19f16a5916
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-15 12:20:47 +0800

    :rocket: singleton

---
+++ singleton/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"Aryzath/singleton/singleton"
+	"fmt"
+	"sync"
+)
+
+var once sync.Once // thread safe
+var singletonInstance *singleton.Singleton
+
+func getInstance() *singleton.Singleton {
+	once.Do(func() {
+		singletonInstance = &singleton.Singleton{}
+		fmt.Println("Created Instance")
+	})
+	return singletonInstance
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go getInstance() // even if we run go routines it only create 1 instance
+	}
+	fmt.Scanln()
+}
