b43b2c5358109d5ce3d48ed83736645ad5e7b383
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-18 16:54:47 +0800

    :rocket: composite

---
+++ composite/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "Aryzath/composite/composite"
+
+func main() {
+	phone := &composite.Product{Name: "IPhone"}
+
+	keyboard := &composite.Product{Name: "Keyboard"}
+	mouse := &composite.Product{Name: "Mouse"}
+
+	oil := &composite.Product{Name: "Oil"}
+
+	box1 := &composite.Box{Name: "box1"}
+	box1.Add(phone)
+
+	box2 := &composite.Box{Name: "box2"}
+	box2.Add(keyboard)
+	box2.Add(mouse)
+	box2.Add(box1)
+
+	box3 := &composite.Box{Name: "box3"}
+	box3.Add(oil)
+
+	box4 := &composite.Box{Name: "parent box"}
+	box4.Add(box1)
+	box4.Add(box2)
+	box4.Add(box3)
+
+	box4.Scan()
+}
