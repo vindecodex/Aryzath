d0bda262d02b344fb965d14a6e4d03c1b654512c
Vincent Villaluna <villalunavincent@gmail.com>
2021-02-19 11:55:46 +0800

    :rocket: super exhaust object

---
+++ decorator/decorator/super_exhaust.go
@@ -0,0 +1,13 @@
+package decorator
+
+import "Aryzath/decorator/component"
+
+type SuperExhaust struct {
+	Car component.Car
+}
+
+func (s *SuperExhaust) Set() []string {
+	car_set := s.Car.Set()
+	car_set = append(car_set, "super exhaust")
+	return car_set
+}
