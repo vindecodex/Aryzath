5f842162a45112ad148adb140391a160bbfd6889
Vincent Villaluna <villalunavincent@gmail.com>
2021-03-02 16:05:47 +0800

    :tada: player

---
+++ flyweight/game/player.go
@@ -0,0 +1,19 @@
+package game
+
+import "Aryzath/flyweight/flyweight"
+
+type Player struct {
+	Team flyweight.Team
+}
+
+func (p *Player) getTeamColor() string {
+	return p.Team.GetColor()
+}
+
+func (p *Player) setTeam(team flyweight.Team) {
+	p.Team = team
+}
+
+func newPlayer() *Player {
+	return &Player{}
+}
