35b71153425845de595e92edc76efe5fa6f26574
Vincent Villaluna <villalunavincent@gmail.com>
2021-03-03 14:58:21 +0800

    :rocket: proxy pattern

---
+++ proxy/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"Aryzath/proxy/proxy"
+	"fmt"
+)
+
+func main() {
+	proxy := proxy.NewProxy()
+
+	fmt.Println(proxy.SetData("user", "Data001"))
+	proxy.SetProxyMode("Set")
+	fmt.Println(proxy.SetData("user", "Data001"))
+	fmt.Println(proxy.SetData("admin", "Data002"))
+
+	fmt.Println(proxy.GetData("user"))
+	proxy.SetProxyMode("Get")
+	fmt.Println(proxy.GetData("user"))
+	fmt.Println(proxy.GetData("admin"))
+
+}
