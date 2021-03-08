dd941e02859e6ff8a82d03bb9251a800d83fd3ee
Vincent Villaluna <villalunavincent@gmail.com>
2021-03-08 14:19:30 +0800

    :rocket: iterator

---
+++ iterator/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"Aryzath/iterator/iterator"
+	"fmt"
+)
+
+func main() {
+	data_a := &iterator.Data{}
+	data_b := &iterator.Data{}
+
+	data_a.SetData("ABC")
+	data_b.SetData("XYZ")
+
+	collections := iterator.DataCollection{Data: []*iterator.Data{data_a, data_b}}
+
+	iterator := collections.CreateIterator()
+
+	for iterator.HasNext() {
+		data := iterator.GetNext()
+		fmt.Printf("Data: $%s \n", data.GetData())
+	}
+}
