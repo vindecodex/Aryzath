f770fc2c848d2ba623dc40d444661592e8016deb
Vincent Villaluna <villalunavincent@gmail.com>
2021-03-10 19:07:26 +0800

    :rocket: observer

---
+++ observer/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "Aryzath/observer/observer"
+
+func main() {
+	customerA := observer.NewCustomer(1, "John")
+	customerB := observer.NewCustomer(2, "Mark")
+
+	store := observer.NewStore("PS5")
+
+	store.Register(customerA)
+	store.Register(customerB)
+	store.Notify()
+
+	customerC := observer.NewCustomer(3, "Andrie")
+	store.Register(customerC)
+	store.Unregister(customerB.GetId())
+	store.Notify()
+}
