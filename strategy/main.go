66cfc844a2f6a90ffc9f829e662649a5ae8b926e
Vincent Villaluna <villalunavincent@gmail.com>
2021-03-12 19:54:00 +0800

    :rocket: strategy

---
+++ strategy/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "Aryzath/strategy/strategy"
+
+func main() {
+	squareRoot := strategy.NewSquareRoot(5)
+
+	stratA := &strategy.StrategyA{}
+	stratB := &strategy.StrategyB{}
+	stratC := &strategy.StrategyC{}
+
+	squareRoot.SetStrategy(stratA)
+	squareRoot.Solve()
+
+	squareRoot.SetStrategy(stratB)
+	squareRoot.Solve()
+
+	squareRoot.SetStrategy(stratC)
+	squareRoot.Solve()
+
+}
