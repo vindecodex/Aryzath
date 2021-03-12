4e2dcbc8defe7d175dae519f1675926f67c36c84
Vincent Villaluna <villalunavincent@gmail.com>
2021-03-12 11:45:13 +0800

    :tada: state

---
+++ state/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"Aryzath/state/state"
+	"log"
+)
+
+func main() {
+	car := state.NewCar(50)
+
+	err := car.StartEngine()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = car.Run()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	car.Break()
+
+	err = car.StopEngine()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+}
