c1380c31ac38cf1d75b76fd7aa8add5f524e7655
vincent villaluna <villalunavincent@gmail.com>
2022-03-28 08:59:14 +0800

    return the function call

--- adapter/adapter/mp4_adapter.go
+++ adapter/adapter/mp4_adapter.go
@@ -10,7 +10,7 @@ type Mp4Adapter struct {
 	Mp4 *file.Mp4
 }
 
-func (mp4Adapter *Mp4Adapter) PlayAudio() { // since Mp4 doesn't have PlayAudio we will be the one to implement that method
+func (mp4Adapter *Mp4Adapter) PlayAudio() string { // since Mp4 doesn't have PlayAudio we will be the one to implement that method
 	fmt.Println("Initializing Adapter...")
-	mp4Adapter.Mp4.PlayVideo()
+	return mp4Adapter.Mp4.PlayVideo()
 }
