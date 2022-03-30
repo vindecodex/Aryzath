9c874d716c892cadf9fd26bef2f63489e06ad0f2
vincent villaluna <villalunavincent@gmail.com>
2022-03-30 12:05:50 +0800

    Add return type for compile

--- bridge/abstraction_heirarchy/programming_language.go
+++ bridge/abstraction_heirarchy/programming_language.go
@@ -3,6 +3,6 @@ package abstraction_heirarchy
 import "github.com/vindecodex/Aryzath/bridge/implementation_heirarchy"
 
 type ProgrammingLanguage interface {
-	Compile()
+	Compile() string
 	SetIde(implementation_heirarchy.Ide)
 }
