c2d1bd5ca86b482b4a8e7e5a0377df31e48c01ed
vincent villaluna <villalunavincent@gmail.com>
2022-03-31 17:23:08 +0800

    update log output

--- builder/main.go
+++ builder/main.go
@@ -13,14 +13,14 @@ func main() {
 	director := builder.NewDirector(crystalBuilder)
 
 	crystalCyborg := director.BuildCyborg()
-	fmt.Println("arms: ", crystalCyborg.LeftArm, " - ", crystalCyborg.RightArm)
+	fmt.Println("arms: ", crystalCyborg.LeftArm, " and ", crystalCyborg.RightArm)
 	fmt.Println("legs: ", crystalCyborg.Legs)
 	fmt.Println("procs:", crystalCyborg.Processor)
 
 	director.SetBuilder(rubyBuilder)
 
 	rubyCyborg := director.BuildCyborg()
-	fmt.Println("arms: ", rubyCyborg.LeftArm, " - ", rubyCyborg.RightArm)
+	fmt.Println("arms: ", rubyCyborg.LeftArm, " and ", rubyCyborg.RightArm)
 	fmt.Println("legs: ", rubyCyborg.Legs)
 	fmt.Println("procs:", rubyCyborg.Processor)
 }
