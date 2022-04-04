fc6a6122d4c347a7b8c4f39b00576d713fa79294
vincent villaluna <villalunavincent@gmail.com>
2022-04-04 19:11:18 +0800

    return string value

--- command/device/light.go
+++ command/device/light.go
@@ -1,13 +1,11 @@
 package device
 
-import "fmt"
-
 type Light struct{}
 
-func (l Light) On() {
-	fmt.Println("ON")
+func (l Light) On() string {
+	return "ON"
 }
 
-func (l Light) Off() {
-	fmt.Println("OFF")
+func (l Light) Off() string {
+	return "OFF"
 }
